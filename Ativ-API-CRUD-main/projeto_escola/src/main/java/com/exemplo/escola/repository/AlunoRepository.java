8e7d90b797437448a46c48f5722078ebbb09d601
David-son-frs <fdavidson991@gmail.com>
2025-05-25 18:48:37 -0300

    Delete Ativ-API-CRUD-main/projeto_escola directory

--- Ativ-API-CRUD-main/projeto_escola/src/main/java/com/exemplo/escola/repository/AlunoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.exemplo.escola.repository;
-
-import com.exemplo.escola.model.Aluno;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AlunoRepository extends JpaRepository<Aluno, Long> {
-}
